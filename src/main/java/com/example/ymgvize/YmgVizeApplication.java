3451d32895bb027c7ddf6ab3ea53827f5ae19698
MehlkaBilen <99501031+MehlkaBilen@users.noreply.github.com>
2024-05-07 13:35:03 +0300

    Initial commit

---
+++ src/main/java/com/example/ymgvize/YmgVizeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ymgvize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmgVizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmgVizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ymgvize/YmgVizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ymgvize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmgVizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
